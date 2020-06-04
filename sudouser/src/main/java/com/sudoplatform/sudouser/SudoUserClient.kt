3af05744c68f71422d1c282d3c4b55145ae6d0bc
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2020-06-04 18:41:16 +1000

    Release 7.3.1

--- sudouser/src/main/java/com/sudoplatform/sudouser/SudoUserClient.kt
+++ sudouser/src/main/java/com/sudoplatform/sudouser/SudoUserClient.kt
@@ -361,7 +361,7 @@ class DefaultSudoUserClient(
         private const val MAX_VALIDATION_DATA_SIZE = 2048
     }
 
-    override val version: String = "1.0"
+    override val version: String = "7.2.1"
 
     /**
      * [KeyManagerInterface] instance needed for cryptographic operations.
