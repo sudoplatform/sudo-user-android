8ef35a253d16fc6d7ed05f25c7c0e347e6848844
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2020-06-30 14:23:22 +1000

    Release 7.7.0

--- sudouser/src/main/java/com/sudoplatform/sudouser/TESTAuthenticationProvider.kt
+++ sudouser/src/main/java/com/sudoplatform/sudouser/TESTAuthenticationProvider.kt
@@ -8,6 +8,11 @@ package com.sudoplatform.sudouser
 
 import android.util.Base64
 import com.sudoplatform.sudokeymanager.KeyManagerInterface
+import org.spongycastle.asn1.x509.SubjectPublicKeyInfo
+import java.security.KeyFactory
+import java.security.interfaces.RSAPrivateCrtKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.RSAPublicKeySpec
 import java.util.*
 
 /**
@@ -32,14 +37,15 @@ class TESTAuthenticationInfo(private val jwt: String) : AuthenticationInfo {
  *
  * @param name provider name. This name will be prepend to the generated UUID in JWT sub.
  * @param privateKey PEM encoded RSA private key.
- * @param publicKey PEM encoded RSA public key.
+ * @param publicKey PEM encoded RSA public key. This is optional. If not provided then will be
+ * derived from the private key.
  * @param keyManager [KeyManagerInterface] instance to use for signing authentication info.
  * @param keyId key ID of the TEST registration key which is obtained from the admin console.
  */
 class TESTAuthenticationProvider(
     private val name: String,
     privateKey: String,
-    publicKey: String,
+    publicKey: String?,
     private val keyManager: KeyManagerInterface,
     private val keyId: String = REGISTER_KEY_NAME
     ) :
@@ -58,11 +64,30 @@ class TESTAuthenticationProvider(
                 .replace("-----END RSA PRIVATE KEY-----", ""), Base64.DEFAULT
         )
 
-        val publicKeyData = Base64.decode(
-            publicKey.replace("\n", "")
-                .replace("-----BEGIN RSA PUBLIC KEY-----", "")
-                .replace("-----END RSA PUBLIC KEY-----", ""), Base64.DEFAULT
-        )
+        val publicKeyData: ByteArray
+        if(publicKey != null) {
+            publicKeyData = Base64.decode(
+                publicKey.replace("\n", "")
+                    .replace("-----BEGIN RSA PUBLIC KEY-----", "")
+                    .replace("-----END RSA PUBLIC KEY-----", ""), Base64.DEFAULT
+            )
+        } else {
+            // Generated the public key from the private key bits.
+            val keySpec = PKCS8EncodedKeySpec(privateKeyData)
+            val factory = KeyFactory.getInstance("RSA")
+
+            val key: RSAPrivateCrtKey = factory.generatePrivate(keySpec) as RSAPrivateCrtKey
+
+            val publicKeySpec =
+                RSAPublicKeySpec(key.modulus, key.publicExponent)
+
+            val publicKeyInfo =
+                SubjectPublicKeyInfo.getInstance(factory.generatePublic(
+                    publicKeySpec
+                ).encoded)
+            val publicKeyPKCS1ASN1 = publicKeyInfo.parsePublicKey()
+            publicKeyData = publicKeyPKCS1ASN1.encoded
+        }
 
         this.keyManager.deleteKeyPair(this.keyId)
         this.keyManager.addKeyPair(privateKeyData, publicKeyData, this.keyId)
